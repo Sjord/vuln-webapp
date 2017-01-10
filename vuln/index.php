a68ca04292b478695fbd112d281aadc26c49a6c4
Fraser Savage <fraser_sav@yahoo.co.uk>
2017-01-10 17:20:30 +0000

    Homepage and nav

--- vuln/index.php
+++ vuln/index.php
@@ -15,6 +15,130 @@
     <body>
         <?php include("includes/nav.php") ?>
 
-        <!-- content here my dude -->
+        <section class="hero is-primary is-bold">
+            <div class="container">
+                <div class="notification is-success has-text-centered">
+                   <p>100th-delivery sale is now on, with shipments 30% off!
+                    <a href="order.php">
+                        <span class="tag">
+                            <strong>Order Now</strong>
+                        </span>
+                    </a> 
+                    </p>
+                </div>
+            </div>
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title is-1">
+                        We are Planet Express, Inc.
+                    </h1>
+                    <h2 class="subtitle is-3">
+                        Our crew is replaceable. Your package isn't.
+                    </h2>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="content">
+                    <h1 class="title">What we are</h1>
+                    <p>
+                        Planet Express, Inc. is an intergalactic delivery company owned and operated by Professor Farnsworth to fund his research. Founded in 2961, our headquarters are located in New New York, and our crew are the most capable in the region.
+                    </p>
+                </div>   
+            </div> 
+			<br>
+            <div class="container">
+				<h1 class="title">Meet the crew</h1>
+				<p>
+					Our crew are well experienced and are guaranteed to put your satisfaction first!	
+				</p>
+                <div class="columns">
+					<div class="column">	
+						<div class="card">
+						  <div class="card-image">
+							<figure class="image is-4by3">
+							  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+							</figure>
+						  </div>
+						  <div class="card-content">
+							<div class="media">
+							  <div class="media-left">
+								<figure class="image" style="height: 40px; width: 40px;">
+								  <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+								</figure>
+							  </div>
+							  <div class="media-content">
+								<p class="title is-4">Turanga Leela</p>
+								<p class="subtitle is-6">@captain</p>
+							  </div>
+							</div>
+
+							<div class="content">
+								<a>@captain</a> is the ship's captain and pilot, leading the regular delivery crew<a>@planetexpress</a>.
+							</div>
+						  </div>
+						</div>
+					</div>
+
+					<div class="column">	
+						<div class="card">
+						  <div class="card-image">
+							<figure class="image is-4by3">
+							  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+							</figure>
+						  </div>
+						  <div class="card-content">
+							<div class="media">
+							  <div class="media-left">
+								<figure class="image" style="height: 40px; width: 40px;">
+								  <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+								</figure>
+							  </div>
+							  <div class="media-content">
+								<p class="title is-4">Philip J. Fry</p>
+								<p class="subtitle is-6">@fry</p>
+								
+							  </div>
+							</div>
+
+							<div class="content">
+							  	<a>@fry</a> is the executive delivery boy for <a>@planetexpress</a>.
+							</div>
+						  </div>
+						</div>
+					</div>
+					
+					<div class="column">	
+						<div class="card">
+						  <div class="card-image">
+							<figure class="image is-4by3">
+							  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
+							</figure>
+						  </div>
+						  <div class="card-content">
+							<div class="media">
+							  <div class="media-left">
+								<figure class="image" style="height: 40px; width: 40px;">
+								  <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
+								</figure>
+							  </div>
+							  <div class="media-content">
+								<p class="title is-4">Bender Bending Rodriguez</p>
+								<p class="subtitle is-6">@shinymetal</p>
+							  </div>
+							</div>
+
+							<div class="content">
+								<a>@shinymetal</a> is a bending unit who works <a>@planetexpress</a> as the company chef.
+							</div>
+						  </div>
+						</div>
+					</div>
+
+                </div>   
+            </div> 
+        </section>
     </body>
 </html>
