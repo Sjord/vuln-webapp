c0186a2381bae8943235d95889e4ec34e2750f94
Ashu <ashu.savani.15@ucl.ac.uk>
2017-01-11 23:41:38 +0000

    added post features

--- profile.php
+++ profile.php
@@ -1,5 +1,7 @@
 <?php
+    require("config.php");
     session_start();
+    $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABSE);
 ?>
 <html>
     <head>
@@ -8,8 +10,29 @@
     </head>
 
     <?php
+        $result = mysqli_query($db, "SELECT * FROM user");
+        $deets = mysqli_fetch_array($result);
         include("includes/nav.php");
-        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true)
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            $firstName = $_POST["firstName"];
+            $lastName = $_POST["lastName"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $userName = $_POST["userName"];
+            $privilege_level = $_POST["privilege_level"];
+            if($db->connect_errno)
+            {
+                echo "Failed to connect to MySQL: (" . $mysqli->db. ") " . $mysqli->db;
+            }
+            else
+            {
+                $db->query("INSERT into 'user' (firstname, lastname, email, password, username, privilege_level) VALUES ('$firstName', '$lastName', '$email', '$password', '$userName', '$privilege_level') ");
+            }
+
+        }
+
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) //check for username, may have to change it
         {
         ?>
 
@@ -22,17 +45,17 @@
                     <div class="control is-horizontal">
                         <div class="control is-grouped">
                             <p class="control is-expanded">
-                                <input class="input" type="text" name="firstName" placeholder="First Name">
+                                <input class="input" type="text" value="<?=$deets['firstname'];?>" name="firstName" placeholder="First Name">
                             </p>
                             <p class="control is-expanded">
-                                <input class="input" type="text" name="lastName" placeholder="Last Name">
+                                <input class="input" type="text" value="<?=$deets['lastname'];?>"name="lastName" placeholder="Last Name">
                             </p>
                         </div>
                     </div>
 
                     <div class="control">
                             <p class="control">
-                                <input class="input" type="email" placeholder="Email" name="email">
+                                <input class="input" type="email" value="<?=$deets['email'];?>" placeholder="Email" name="email">
                             </p>
                     </div>
 
@@ -48,7 +71,11 @@
                     </div>
 
                     <p class="control">
-                        <input class="input" type="hidden" placeholder="username">
+                        <input class="input"  name="userName" value="<?=$deets['username'];?>" placeholder="username">
+                    </p>
+
+                    <p class="control">
+                        <input class="input" type="hidden"  name="privilege_level"  placeholder="privilege level">
                     </p>
 
                     <p class = "control">
@@ -56,9 +83,6 @@
                             Submit
                         </button>
                     </p>  
-
-
-
         }
 
 
