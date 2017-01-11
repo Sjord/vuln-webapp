6bb95f14a956ff981e494ea6969a4242970ed1c9
Matthew Bell <logicnotlogical@outlook.com>
2017-01-11 16:11:29 +0000

    Restore deleted php files

---
+++ login.php
@@ -0,0 +1,90 @@
+<?php
+    require("config.php");
+    session_start();
+    if (isset($_SESSION['username']))
+    {
+        header("Location: index.php");
+    }
+
+    $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
+<!doctype html>
+<html>
+    <head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.0/css/bulma.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    </head>
+
+    <?php
+    //include("/includes/nav.php");
+    //include the configuration file with database access
+    //also add connection from php to the sql database(which will probs be in the config file)
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username'] && isset($_POST['password'])))
+    {
+        $myusername = $_POST["username"];
+        $mypassword = $_POST["password"];
+
+        $sql = "SELECT id FROM admin WHERE username = $myusername AND password = $mypassword";
+        $result = $db->query($sql);
+        $row = $db->fetch_array(MYSQLI_ASSOC);
+        $active = $row["active"];
+        //if result matched username and password, table row must be 1
+        
+        if($result->num_rows > 0)
+        {
+            $_SESSION["username"] = $myusername;
+            header("Location: index.php");
+        }
+        else
+        {
+            header("Location: login.php?invalid");
+        }  
+    }
+    ?>
+    
+
+    <body>
+        <h1 class="title is-one"> Planet Express </h1>
+        <div class="box">
+            <?php
+                if isset($_GET['invalid'])
+                {
+                     echo "The username or password you entered is invalid.";
+                }
+            ?>
+            <form method="POST">
+                <p class="control has-icon">
+                    <input class="input is-primary" name="username" type="text" placeholder="Username">
+                    <span class="icon is-small">
+                        <i class="fa fa-envelope"></i>
+                    </span>
+                </p>
+                <p class="control has-icon">
+                    <input class="input is-danger" name="password" type="password" placeholder="Password">
+                    <span class="icon is-small">
+                        <i class="fa fa-lock">
+                    </span>
+                </p>
+                <p class="control">
+                    <button class="button is-success" type="submit">
+                        Login
+                    </button>
+                </p> 
+                <br>
+                <br>
+                <p class = "control">
+                    <button class="button is-success">
+                        Forgot Username
+                    </button>
+                </p> 
+                <p class = "control">
+                    <button class="button is-success">
+                        Forgot Password
+                    </button>
+                </p> 
+            </form>
+        </div>
+    </body>
+
+
+
---
+++ signup.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+    <head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.0/css/bulma.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    </head>
+
+    <?php
+    //include("/includes/nav.php");
+    //include("config.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $email = $_POST["email"];
+        $pwrod = $_POST["password"];
+
+        //define $query depending on tables and databases matt adds
+
+        if(mysqli_query($db, $query))
+        {
+            echo "Records added successfully";
+        }
+
+        else
+        {
+            echo "ERROR: Could not be able to execute $sql" . mysqli_error($db);
+        }
+    }
+    ?>
+
+    <body>
+        <h1 class="title is-one"> Planet Express </h1>
+        <h3 class="title is-three"> Sign Up Here </h1>
+        <form method="POST">
+            <div class="control is-horizontal">
+                <div class="control is-grouped">
+                    <p class="control is-expanded">
+                        <input class="input" type="text" name="firstName" placeholder="First Name">
+                    </p>
+                    <p class="control is-expanded">
+                        <input class="input" type="text" placeholder="Last Name">
+                    </p>
+                </div>
+            </div>
+
+            <div class="control">
+                    <p class="control">
+                        <input class="input" type="email" placeholder="Email" name="email">
+                    </p>
+            </div>
+
+            <div class="control is-horizontal">
+                <div class="control is-grouped">
+                    <p class="control is-expanded">
+                        <input class="input" type="password" placeholder="Password" name="password">
+                    </p>
+                    <p class="control is-expanded">
+                        <input class="input" type="password" placeholder="Enter Password">
+                    </p>
+                </div>
+            </div>
+
+            <p class="control">
+                <input class="input" type="hidden" placeholder="username">
+            </p>
+
+            <p class = "control">
+                <button class="button is-success" type="submit">
+                    Submit
+                </button>
+            </p> 
+
+
+
